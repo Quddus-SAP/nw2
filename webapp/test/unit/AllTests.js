23f7be0404af8fd0ce864abd19dbfe99ca7e983f
Mohammed Quddus <quddus_sap@outlook.com>
2023-07-11 06:35:59 +0000

    first commit

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"s/qu_baby_nw2/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ webapp/test/unit/controller/View1.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"s/qu_baby_nw2/controller/View1.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("View1 Controller");
+
+	QUnit.test("I should test the View1 controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
